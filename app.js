486ae0d67793ff118eb905ccfaeb2d814caae1ec
Seth DImmer <sethdimmer1@gmail.com>
2023-02-18 16:54:31 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,2 +1,70 @@
 // local reviews data
 
+// i am setting up data locally because i havcent learned ajax or how to setup
+// http requests
+
+// thta is why i will use local data in these DOM PROJECTS
+
+const reviews = [ /*array of objects*/
+// 4 items
+    {
+      id: 1,
+      name: 'susan smith',
+      job: 'web developer',
+      img: 'https://images2.imgbox.com/e0/57/qI5bbwvg_o.jpeg',
+      text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: 'anna johnson',
+      job: 'web designer',
+      img: 'https://images2.imgbox.com/2e/6e/JAMvTZ56_o.jpeg',
+      text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    },
+    {
+      id: 3,
+      name: 'peter jones',
+      job: 'intern',
+      img: 'https://images2.imgbox.com/56/88/oJvFN3l5_o.jpeg',
+      text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    },
+    {
+      id: 4,
+      name: 'bill anderson',
+      job: 'the boss',
+      img: 'https://images2.imgbox.com/8b/1c/vwWNTsCd_o.jpeg',
+      text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    },
+  ];
+
+//   what do i want to happen.
+
+// what i want to do is i want to access the items from the html
+// meaning i  want to access the image,author,and job info 
+
+// i want to change the information DYNAMICALLY when i click one of the arrow buttons
+
+//  as well as have a math.random on the surpise me button to generate the random a  object 
+// in the array of 4 that i have.
+
+// select items
+
+//  storing them in variables so it is easier to access them 
+
+const img = document.getElementById('person-img')
+const author = document.getElementById('author')
+const job = document.getElementById('job')
+const info = document.getElementById('info')
+
+const prevBtn = document.querySelector('.prev-btn') /*accessing classes i use query selector*/
+const nextBtn = document.querySelector('.next-btn')
+const randomBtn = document.querySelector('.random-btn')
+
+//  i need to setup some sort of value in my array 
+// so i will start with 0 index since it is an array
+
+
+
+
+
+
