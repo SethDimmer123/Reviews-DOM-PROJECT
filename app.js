b34d02f25fe02414434e37b02be2fa9d4a3c4fd1
Seth DImmer <sethdimmer1@gmail.com>
2023-02-19 19:57:03 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 // local reviews data
-
-// i am setting up data locally because i haven't learned ajax or how to setup
-// http requests.
-// that is why i will use local data in these DOM PROJECTS
-
-const reviews = [ /*array of objects*/
-// 4 items
+const reviews = [
     {
       id: 1,
       name: 'susan smith',
@@ -36,31 +30,34 @@ const reviews = [ /*array of objects*/
     },
   ];
 
-//   what do i want to happen.
+//   i need to figure out what i want to happen?
 
-// what i want to do is i want to access the items from the html
-// meaning i want to access the image,author,and job info .
+
+// what i want to happen is i want to access the items from the html
+
+// I WANT TO ACCESS THE image,author,and job info.
 
 // i want to change the information DYNAMICALLY when i click one of the arrow buttons
 
 //  as well as have a math.random on the Surpise Me button to generate the random a object 
 // in the array of 4 that i have.
 
-// select items
+
+  // select items
 
 //  storing them in variables so it is easier to access them 
 
-const img = document.getElementById('person-img')
-const author = document.getElementById("author")
-const job = document.getElementById('job')
-const info = document.getElementById('info')
+  const img = document.getElementById('person-img');
+  const author = document.getElementById('author');
+  const job = document.getElementById('job');
+  const info = document.getElementById('info');
 
-/*accessing classes i use query selector*/
+  /*accessing classes i use query selector*/
+  
+  const prevBtn = document.querySelector('.prev-btn');
+  const nextBtn = document.querySelector('.next-btn');
+  const randomBtn = document.querySelector('.random-btn');
 
-
-const prevBtn = document.querySelector('.prev-btn') 
-const nextBtn = document.querySelector('.next-btn')
-const randomBtn = document.querySelector('.random-btn')
 
 
 //  now i need to setup some sort of variable that stores my starting value in my array.
@@ -68,13 +65,11 @@ const randomBtn = document.querySelector('.random-btn')
 
 // i have 4 items in the array
 
-
 // set starting item with an index of 0 which is the first item 
 // object in the array.
-let currentItem = 0;
+  let currentItem = 0;
 
-
-// now what do i want to do?
+  // now what do i want to do?
 
 // once my document loads i want to access whatever item from the array which 
 // in this case is the first object with the id of 1. 
@@ -83,9 +78,9 @@ let currentItem = 0;
 
 // then with all of the variables i have i will replace the values of the id 
 // and classes from the index.html (DYNAMICALLY).
+  
+  // load initial item
 
-
-// load initial item(5:30)
 // we add event listener on the window object because i am adding it to the current window 
 // (tab) i am on.
 
@@ -95,37 +90,24 @@ let currentItem = 0;
 // when the DOMContentLoaded has been loaded then the code INSIDE of the function
 // is executed.
 
-window.addEventListener('DOMContentLoaded',function(){
-    // console.log('seth')
-    showPerson(currentItem)
-
-    // but now i want to access my first item how do i do that?(6:20)
-
+  window.addEventListener('DOMContentLoaded', function () {
     const item = reviews[currentItem];
 
-    // i now have access to my first item in my array which is 0 by using the 
-    // reviews array and put it in a variable which is item.
-    // and pass in my currentItem in the reviews array
-
-    img.src = item.img
-
-    // for image i have the src property
+    img.src = item.img;
+        // for image i have the src property
     // i set it equal to my image because i assigned the first object(item) with
     // the id of 1 
     // in my first object(item) i have a the img property which gives me the image
     // of that object
-
     author.textContent = item.name;
     job.textContent = item.job;
     info.textContent = item.text;
-
-    // now since i have the info for 1 item i want to change the info based on
+        // now since i have the info for 1 item i want to change the info based on
     // the functinaility of the 2 buttons.(10:30) increasing or decreasing the currentItem
     // variable on (line 76)
+  });
 
-
-
-    // (10:52) BEFORE I increase or decrease the currentItem value which is 
+      // (10:52) BEFORE I increase or decrease the currentItem value which is 
     // at this point 0
     // i have REFACTOR MY CODE IN THE event listener above becuase 
     // even though the code is right when i increase or decrease the item number 
@@ -140,48 +122,45 @@ window.addEventListener('DOMContentLoaded',function(){
 
     // HOW TO DO THAT DOWN BELOW
 
-})
 
-// show person based on item number (11:44)
-
-function showPerson(number) {
-    const item = reviews[number];
-    item.src = item.img;
+  
+  // show person based on item (11:44)
+  function showPerson(person) {
+    const item = reviews[person];
+    img.src = item.img;
     author.textContent = item.name;
     job.textContent = item.job;
     info.textContent = item.text;
-}
+  }
 
-// then i call the function by putting in the DOMContentLoaded function.
+// then i call the function by putting the currentItem of the reviews which is 0(line 70)
+// which is also the starting index in a variable to use to access the image,author,job info
+// (lines 96,102,103,104) in the DOMContentLoaded function.(line 93)
 // then pass in the currentItem variable so i can reuse the showPerson function
 
 
 // i can now setup 2 event listeners for my 2 arrow buttons
 
-// show next person
-
-nextBtn.addEventListener('click',function(){
-    currentItem++
-    if(currentItem > reviews.length - 1){ // the -1 reflects the actual last item in my array
+  // show next person
+  nextBtn.addEventListener('click', function () {
+    currentItem++;
+    if (currentItem > reviews.length - 1) {// the -1 reflects the actual last item in my array
         // if the currentItem is GREATER than the length of the reviews array 
         // if im at the last item in the array then i will start from scratch
-        currentItem = 0
+      currentItem = 0;
     }
-    // if i am at my last item in the array thenthe currentItem is 0 so start from scratch
-    showPerson(currentItem)
-})
-
-
-prevBtn.addEventListener('click',function(){
+        // if i am at my last item in the array thenthe currentItem is 0 so start from scratch
+    showPerson(currentItem);
+  });
+  // show prev person
+  prevBtn.addEventListener('click', function () {
     currentItem--;
-    if(currentItem < 0){ // the -1 reflects the actual last item in my array
-        currentItem = reviews.length - 1;
+    if (currentItem < 0) { // the -1 reflects the actual last item in my array
+      currentItem = reviews.length - 1;
     }
     // if i am at my last item in the array then the currentItem is 0 so start from scratch
-    showPerson()
-
-})
-
+    showPerson(currentItem);
+  });
 
 
     // challenge for random suprise me button(19:00)
@@ -193,11 +172,14 @@ prevBtn.addEventListener('click',function(){
     // i need to display a random item from the reviews array
 
 
-    randomBtn.addEventListener('click',function(){
-        currentItem = Math.floor(Math.random() * reviews.length)
-        // console.log(currentItem);
-        showPerson(currentItem) 
-    })
+  // show random person
+  randomBtn.addEventListener('click', function () {
+    console.log('hello');
+  
+    currentItem = Math.floor(Math.random() * reviews.length);
+    console.log(currentItem);
+    showPerson(currentItem);
+  });
     
 
 
